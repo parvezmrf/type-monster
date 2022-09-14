88025072ae88f23ca8596823ca5e89185e50fbe6
unknown <parvezmrf@gmail.com>
2022-09-14 21:57:45 +0600

    modal position update

--- script.js
+++ script.js
@@ -107,20 +107,21 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
-    if (count == 0) {
+    if (count == 2) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
+      countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
       startTime = new Date().getTime();
     }
     count--;
-  }, 1000);
+  }, 100);
+
 };
 
 // START Countdown
